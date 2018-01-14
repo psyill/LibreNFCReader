240add95e7306af5d1527d1960b89ba1dda430bc
Isaac Warwick <isa.warwick@gmail.com>
2018-01-14 15:51:43 +0100

    Added permission for NFC.

--- app/src/main/java/com/example/isaacwarwick/nfcreader/MainActivity.java
+++ app/src/main/java/com/example/isaacwarwick/nfcreader/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.isaacwarwick.nfcreader;
 
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if(nfcAdapter != null && nfcAdapter.isEnabled()) {
+            Toast.makeText(this, "NFC available!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "NFC not available!", Toast.LENGTH_LONG).show();
+        }
     }
 }
